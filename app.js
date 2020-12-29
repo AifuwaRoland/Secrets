a2662ba255a0b12b0f1ec3f39db7e6e122dd8417
Roland O. Aifuwa <siroapple10@gmail.com>
2020-12-29 00:26:33 -0600

    added google aoth using passport

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const mongoose = require("mongoose");
 const session = require('express-session');
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const  GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate=require('mongoose-findOrCreate');
+
 
 // const bcrypt = require("bcrypt");
 // const saltRounds = 10;
@@ -47,6 +50,19 @@ const User = new mongoose.model("User", userSchema);
 passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret:process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user)               {
+     return cb(err, profile);
+    });
+  }
+));
 
 app.get("/", function (req, res) {
     res.render("home");
