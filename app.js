39cf8457f846f5ba0e2b279920c89bef00fa6687
Roland O. Aifuwa <siroapple10@gmail.com>
2020-12-28 20:15:17 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,157 @@
+//jshint esversion:6
+require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+// const bcrypt = require("bcrypt");
+// const saltRounds = 10;
+
+// const md5 =require("md5"); // using hashing
+// const encrypt= require("mongoose-encryption");
+
+
+const app = express();
+console.log(process.env.SECRET); // tap to .env
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+mongoose.connect("mongodb://localhost:27017/userDB", { useUnifiedTopology: true, useNewUrlParser: true });
+mongoose.set("useCreateIndex", true);
+
+app.use(session({
+    secret: "our little secret.",
+    resave: false,
+    saveUnitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+userSchema.plugin(passportLocalMongoose); // does the hasing and salting then saves it on DB
+
+
+// userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password'] }); //encryption for password
+
+const User = new mongoose.model("User", userSchema);
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.get("/", function (req, res) {
+    res.render("home");
+});
+app.get("/login", function (req, res) {
+    res.render("login");
+
+});
+app.get("/register", function (req, res) {
+    res.render("register");
+
+});
+app.get("/secrets", function (req, res) {
+    if (req.isAuthenticated()) {
+        res.render("secrets");
+    } else {
+        res.redirect("/login");
+    }
+});
+app.get("/logout", function(req, res){
+    req.logOut();
+    res.redirect("/");
+})
+app.post("/register", function (req, res) {
+
+
+    User.register({ username: req.body.username }, req.body.password, function (err, user) {
+        if (err) {
+            console.log(err);
+            res.redirect("/register");
+        } else {
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/secrets");
+            })
+        }
+
+
+
+
+
+
+
+
+
+        // bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+
+        //     const newUser = new User({
+        //         email: req.body.username,
+        //         password: hash
+        //     });
+        //     newUser.save(function (err) {
+        //         if (err) {
+        //             console.log(err);
+        //         } else {
+        //             res.render("secrets");
+        //         }
+        //     });
+        // });
+
+
+    });
+});
+app.post("/login", function (req, res) {
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    req.login(user, function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            passport.authenticate("local");
+            res.redirect("/secrets");
+        }
+    })
+
+
+
+
+
+    // const username = req.body.username;
+    // const password = (req.body.password);
+
+    // User.findOne({ email: username }, function (err, foundUser) {
+    //     if (err) {
+    //         console.log(err);
+    //     } else {
+    //         if (foundUser) {
+    //             // if (foundUser.password === password) { // for hashing
+    //             bcrypt.compare(password, foundUser.password, function (err, result) {
+    //                 if (result === true) {
+    //                     res.render("secrets");
+
+    //                 }
+    //             });
+
+    //         }
+    //     }
+    // });
+
+});
+
+
+app.listen(3000, function (req, res) {
+
+    console.log("Server has started Sucessfully");
+});
